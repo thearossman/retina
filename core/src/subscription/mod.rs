42e9297528674134fab8ea9921a95568b211a08e
Thea Rossman <thearossman@gmail.com>
2025-08-20 16:48:21 +0000

    sketch of approach that uses enums

--- core/src/subscription/mod.rs
+++ core/src/subscription/mod.rs
@@ -14,6 +14,8 @@ pub mod quic_stream;
 pub mod tls_handshake;
 pub mod zc_frame;
 
+use std::any::Any;
+
 // Re-export subscribable types for more convenient usage.
 pub use self::connection::Connection;
 pub use self::connection_frame::ConnectionFrame;
@@ -25,7 +27,7 @@ pub use self::tls_handshake::TlsHandshake;
 pub use self::zc_frame::ZcFrame;
 
 use crate::conntrack::conn_id::FiveTuple;
-use crate::conntrack::pdu::L4Pdu;
+use crate::conntrack::pdu::{L4Context, L4Pdu};
 use crate::conntrack::ConnTracker;
 use crate::filter::{ConnFilterFn, PacketFilterFn, SessionFilterFn};
 use crate::filter::{FilterFactory, FilterResult};
@@ -48,6 +50,123 @@ pub enum Level {
     Session,
 }
 
+pub struct SubscribableWrapper {
+    subscriptions: Vec<SubscribedTypeEnum>,
+    callbacks: Vec<Box<dyn Fn(dyn Any)>>,
+}
+
+impl SubscribableWrapper {
+    pub fn new(
+        subscriptions: Vec<SubscribedTypeEnum>,
+        callbacks: Vec<Box<dyn Fn(dyn Any)>>
+    ) -> Self {
+        SubscribableWrapper {
+            subscriptions,
+            callbacks,
+        }
+    }
+
+    fn level(&self) -> Level {
+        let levels = self.subscriptions
+            .iter()
+            .map(|s| s.level())
+            .collect::<Vec<>>();
+        if levels.iter().any(|&l| l == Level::Connection) {
+            return Level::Connection;
+        }
+        if levels.iter().any(|&l| l == Level::Session) {
+            return Level::Session;
+        }
+        Level::Packet
+    }
+
+    fn parsers() -> Vec<ConnParser> {
+        let mut parsers = Vec::new();
+        for subscription in &self.subscriptions {
+            for parser in subscription.parsers() {
+                if !parsers.contains(&parser) {
+                    parsers.push(parser);
+                }
+            }
+        }
+        parsers
+    }
+
+    fn process_packet(
+        mbuf: Mbuf,
+        subscription: &Subscription,
+        callbacks: &SubscribableWrapper,
+        conn_tracker: &mut ConnTracker,
+    ) where
+        Self: Sized
+    {
+        match subscription.filter_packet(&mbuf) {
+            FilterResult::MatchTerminal(idx) | FilterResult::MatchNonTerminal(idx) => {
+                if let Ok(ctxt) = L4Context::new(&mbuf, idx) {
+                    conn_tracker.process(mbuf, ctxt, subscription);
+                }
+            }
+            FilterResult::NoMatch => drop(mbuf),
+        }
+    }
+}
+
+// Gross workaround (can't do `dyn` with associated types),
+// but okay for naive solution
+pub enum SubscribedTypeEnum {
+    Connection,
+    ConnectionFrame,
+    DnsTransaction,
+    Frame,
+    HttpTransaction,
+    QuicStream,
+    TlsHandshake,
+    ZcFrame,
+}
+
+impl SubscribedTypeEnum {
+    fn level(&self) -> Level {
+        match self {
+            SubscribedTypeEnum::Connection => Connection::level(),
+            SubscribedTypeEnum::ConnectionFrame => ConnectionFrame::level(),
+            SubscribedTypeEnum::DnsTransaction => DnsTransaction::level(),
+            SubscribedTypeEnum::Frame => Frame::level(),
+            SubscribedTypeEnum::HttpTransaction => HttpTransaction::level(),
+            SubscribedTypeEnum::QuicStream => QuicStream::level(),
+            SubscribedTypeEnum::TlsHandshake => TlsHandshake::level(),
+            SubscribedTypeEnum::ZcFrame => ZcFrame::level(),
+        }
+    }
+
+    fn parsers(&self) -> Vec<ConnParser> {
+        match self {
+            SubscribedTypeEnum::Connection => Connection::parsers(),
+            SubscribedTypeEnum::ConnectionFrame => ConnectionFrame::parsers(),
+            SubscribedTypeEnum::DnsTransaction => DnsTransaction::parsers(),
+            SubscribedTypeEnum::Frame => Frame::parsers(),
+            SubscribedTypeEnum::HttpTransaction => HttpTransaction::parsers(),
+            SubscribedTypeEnum::QuicStream => QuicStream::parsers(),
+            SubscribedTypeEnum::TlsHandshake => TlsHandshake::parsers(),
+            SubscribedTypeEnum::ZcFrame => ZcFrame::parsers(),
+        }
+    }
+}
+
+pub enum TrackedTypeEnum {
+    Connection(Box<connection::TrackedConnection>),
+    ConnectionFrame(Box<connection_frame::TrackedConnectionFrame>),
+    DnsTransaction(Box<dns_transaction::TrackedDns>),
+    Frame(Box<frame::TrackedFrame>),
+    HttpTransaction(Box<http_transaction::TrackedHttp>),
+    QuicStream(Box<quic_stream::TrackedQuic>),
+    TlsHandshake(Box<tls_handshake::TrackedTls>),
+    ZcFrame(Box<zc_frame::TrackedZcFrame>),
+}
+
+pub struct TrackedWrapper {
+    tracked: Vec<Box<TrackedTypeEnum>>,
+}
+
 /// Represents a generic subscribable type. All subscribable types must implement this trait.
 pub trait Subscribable {
     type Tracked: Trackable<Subscribed = Self>;
@@ -61,7 +180,7 @@ pub trait Subscribable {
     /// Process a single incoming packet.
     fn process_packet(
         mbuf: Mbuf,
-        subscription: &Subscription<Self>,
+        subscription: &Subscription,
         conn_tracker: &mut ConnTracker<Self::Tracked>,
     ) where
         Self: Sized;
@@ -79,40 +198,35 @@ pub trait Trackable {
     fn pre_match(&mut self, pdu: L4Pdu, session_id: Option<usize>);
 
     /// Update tracked subscription data on a full filter match.
-    fn on_match(&mut self, session: Session, subscription: &Subscription<Self::Subscribed>);
+    fn on_match<F>(&mut self, session: Session, callback: F)
+        where F: Fn(&dyn Any);
 
     /// Update tracked subscription data after a full filter match.
-    fn post_match(&mut self, pdu: L4Pdu, subscription: &Subscription<Self::Subscribed>);
+    fn post_match<F>(&mut self, pdu: L4Pdu, callback: F)
+        where F: Fn(&dyn Any);
 
     /// Update tracked subscription data on connection termination.
-    fn on_terminate(&mut self, subscription: &Subscription<Self::Subscribed>);
+    fn on_terminate<F>(&mut self, callback: F)
+        where F: Fn(&dyn Any);
 }
 
 /// A request for a callback on a subset of traffic specified by the filter.
 #[doc(hidden)]
-pub struct Subscription<'a, S>
-where
-    S: Subscribable,
-{
+pub struct Subscription {
     packet_filter: PacketFilterFn,
     conn_filter: ConnFilterFn,
     session_filter: SessionFilterFn,
-    callback: Box<dyn Fn(S) + 'a>,
     #[cfg(feature = "timing")]
     pub(crate) timers: Timers,
 }
 
-impl<'a, S> Subscription<'a, S>
-where
-    S: Subscribable,
-{
+impl Subscription {
     /// Creates a new subscription from a filter and a callback.
-    pub(crate) fn new(factory: FilterFactory, cb: impl Fn(S) + 'a) -> Self {
+    pub(crate) fn new(factory: FilterFactory) -> Self {
         Subscription {
             packet_filter: factory.packet_filter,
             conn_filter: factory.conn_filter,
             session_filter: factory.session_filter,
-            callback: Box::new(cb),
             #[cfg(feature = "timing")]
             timers: Timers::new(),
         }
@@ -133,11 +247,14 @@ where
     pub(crate) fn filter_session(&self, session: &Session, idx: usize) -> bool {
         (self.session_filter)(session, idx)
     }
+}
 
-    /// Invoke the callback on `S`.
-    pub(crate) fn invoke(&self, obj: S) {
-        tsc_start!(t0);
-        (self.callback)(obj);
-        tsc_record!(self.timers, "callback", t0);
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_subscribable_wrapper() {
+
     }
 }
